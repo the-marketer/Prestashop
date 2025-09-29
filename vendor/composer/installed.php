16e7b1fb82612dd70eb3bb7f77af840cb92b001f
Cosmin <cosmin.oancea@themarketer.com>
2025-09-29 17:49:28 +0300

    v1.1.3.1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mktr/themarketer-prestashop',
+        'pretty_version' => 'dev-latest',
+        'version' => 'dev-latest',
+        'reference' => '5d63f17dc3e16b645a65491f9f07e51ac14361d3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mktr/themarketer-prestashop' => array(
+            'pretty_version' => 'dev-latest',
+            'version' => 'dev-latest',
+            'reference' => '5d63f17dc3e16b645a65491f9f07e51ac14361d3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'prestashop/module-lib-mbo-installer' => array(
+            'pretty_version' => 'v3.0.0',
+            'version' => '3.0.0.0',
+            'reference' => '46fbb457e9241b4f8b0c3f9fdabcb9feba0d0303',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../prestashop/module-lib-mbo-installer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
